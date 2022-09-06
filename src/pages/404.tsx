370f72a91ae0dd7bda30c23807c4661cba8a3ca0
Teemu Sirkiä <teemu.sirkia@gmail.com>
2022-09-06 18:52:45 +0300

    Fix link in error pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,10 +3,10 @@ export default function Custom404() {
     <>
       <h1 className="mb-4">404</h1>
       <p>
-        Päädyit sivulle, jota ei ole olemassa. Palaa takaisin <a href="/koodisailo">etusivulle</a>.
+        Päädyit sivulle, jota ei ole olemassa. Palaa takaisin <a href="/neuvontajono">etusivulle</a>.
       </p>
       <p>
-        This page does not exist. Go back to <a href="/koodisailo">home page</a>.
+        This page does not exist. Go back to <a href="/neuvontajono">home page</a>.
       </p>
     </>
   );
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -3,10 +3,10 @@ export default function Custom500() {
     <>
       <h1 className="mb-4">500</h1>
       <p>
-        Palvelimella tapahtui odottamaton virhetilanne. Palaa takaisin <a href="/koodisailo">etusivulle</a>.
+        Palvelimella tapahtui odottamaton virhetilanne. Palaa takaisin <a href="/neuvontajono">etusivulle</a>.
       </p>
       <p>
-        An unexpected server error occurred. Go back to <a href="/koodisailo">home page</a>.
+        An unexpected server error occurred. Go back to <a href="/neuvontajono">home page</a>.
       </p>
     </>
   );
